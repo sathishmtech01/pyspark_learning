0d0a93562db6a81a9a8327c386a47817c86c8c35
sathish <sathish.chandran@indiqube.com>
2018-05-20 12:06:58 +0530

    python basics added

---
+++ 1.Python_Basics.py
@@ -0,0 +1,13 @@
+#1. variable declaration
+#2. python data structure - list,dictionary,tuple
+#3. Control flow & Looping
+#4. fuctions
+#5. class
+# https://www.tutorialspoint.com/python/index.htm
+for i in range(0,100):
+    print(i)
+
+a=(0,1)
+a1=[0,1]
+a2={"key":"value"}
+
--- 1.Spark_Rdd.py
+++ 1.Spark_Rdd.py
@@ -10,12 +10,15 @@ sc = SparkContext(conf=conf)
 print(sc)
 
 # data download
-f = requests.get ("http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data_10_percent.gz")
-with open("data/kddcup.data_10_percent.gz","wb") as file:
-    file.write(f.content)
+# f = requests.get ("http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data_10_percent.gz")
+# with open("data/kddcup.data_10_percent.gz","wb") as file:
+#     file.write(f.content)
 
-
+# data
 data_file = "data/kddcup.data_10_percent.gz"
+# transform
 raw_data = sc.textFile(data_file)
-
+print(raw_data)
+# action
 print(raw_data.count())
+
