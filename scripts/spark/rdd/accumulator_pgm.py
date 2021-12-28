837130ef2a752e4cd4e18ab936b7048e4daa6364
csk <sathish.mtech01@gmail.com>
2021-12-28 13:58:18 +0530

    accumulator and broadcast added

---
+++ scripts/spark/rdd/accumulator_pgm.py
@@ -0,0 +1,27 @@
+import os
+import pyspark
+from pyspark.sql import SparkSession
+from datetime import datetime, date
+import pandas as pd
+from pyspark.sql import Row
+from pyspark.sql.functions import upper,pandas_udf,PandasUDFType,udf
+from pyspark.sql.types import *
+os.environ['SPARK_HOME'] = "/home/csk/sparkscala/spark-2.4.0-bin-hadoop2.6/"
+spark = SparkSession.builder.appName('accumulatorrr').getOrCreate()
+
+accum=spark.sparkContext.accumulator(0)
+rdd=spark.sparkContext.parallelize([1,2,3,4,5])
+rdd.foreach(lambda x:accum.add(x))
+print(accum.value)
+
+accuSum=spark.sparkContext.accumulator(0)
+def countFun(x):
+    global accuSum
+    accuSum+=x
+rdd.foreach(countFun)
+print(accuSum.value)
+
+accumCount=spark.sparkContext.accumulator(0)
+rdd2=spark.sparkContext.parallelize([1,2,3,4,5])
+rdd2.foreach(lambda x:accumCount.add(1))
+print(accumCount.value)
